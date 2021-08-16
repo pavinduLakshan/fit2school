e90c7e6c66a76d2daee726aa11ce1678fe5fa308
pavinduLakshan <174084F@uom.lk>
2021-08-16 15:52:39 +0530

    add todo and markdown rendering support

--- README.md
+++ README.md
@@ -3,4 +3,7 @@ A Knowledge Sharing Community Project By FIT UOM LK
 
 ### TODO
 
-https://dev.to/sadra/what-is-the-best-comment-service-for-a-hosted-website-35h9  
+- Add open source commenting system - https://dev.to/sadra/what-is-the-best-comment-service-for-a-hosted-website-35h9  
+- Implement Github Login
+- Implement markdown article editor 
+- Integrate Imgur API
--- components/markdown.tsx
+++ components/markdown.tsx
@@ -1,30 +1,30 @@
 import ReactMarkdown from 'react-markdown';
-import { NormalComponents, SpecialComponents } from 'react-markdown/lib/ast-to-react';
-import { materialLight } from 'react-syntax-highlighter/dist/cjs/styles/prism';
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import coy  from "react-syntax-highlighter/dist/cjs/prism";
 import { FunctionComponent } from 'react';
 
 interface IProps {
     content: string;
 }
 
+interface codeBlockProps {
+    value: string;
+    language:string;
+}
+
+const CodeBlock: FunctionComponent<codeBlockProps> = ({value,language}) => {
+
+    return <SyntaxHighlighter language={language} style={coy}>
+    {value}
+  </SyntaxHighlighter>
+}
+
 const Markdown: FunctionComponent<IProps> = ({content}) => {
-    const components: Partial<NormalComponents & SpecialComponents> = {
-        code({node, inline, className, children, ...props}) {
-            const match = /language-(\w+)/.exec(className || '');
-    
-            return (!inline && match) ? (
-                <SyntaxHighlighter style={materialLight} PreTag="div" language={match[1]} children={String(children).replace(/\n$/, '')} {...props} />
-            ) : (
-                <code className={className ? className : ""} {...props}>
-                    {children}
-                </code>
-            )
-        }
-    }
 
     return <div className="markdown-body">
-        <ReactMarkdown components={components} children={content} />
+        <ReactMarkdown>
+        {content}
+        </ReactMarkdown>
     </div>
 }
 
